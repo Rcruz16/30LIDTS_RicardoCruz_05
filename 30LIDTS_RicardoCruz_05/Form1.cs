e424bba8ed51298e46d5e9fc3e326ce61e887eb5
Ricardo Uriel Crz Crz <ricardo.cruze@outlook.com>
2023-10-19 09:17:57 -0600

    VERSION FINAL;

--- 30LIDTS_RicardoCruz_05/Form1.cs
+++ 30LIDTS_RicardoCruz_05/Form1.cs
@@ -1,5 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
 namespace _30LIDTS_RicardoCruz_05
@@ -7,7 +17,7 @@ namespace _30LIDTS_RicardoCruz_05
     public partial class Form1 : Form
     {
         //Datos de conexion a Mysql
-        //string conexionSQL = 
+        string conexionSQL = "Server= localhost;Port=3306;Database = programacionavanzada; Uid=root; Pwd=;";
         // Metodo para insertar registros
         public Form1()
         {
@@ -19,15 +29,7 @@ namespace _30LIDTS_RicardoCruz_05
             tb_Nombre.TextChanged += ValidarNombre;
             tb_Apellido.TextChanged += ValidarApellidos;
         }
-        public void ValidarEdad(object sender, EventArgs e)
-        {
-            TextBox textbox = (TextBox)sender;
-            if (!EsEnteroValido(textbox.Text))
-            {
-                MessageBox.Show("Por favor, ingrese una edad válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                textbox.Clear();
-            }
-        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -36,6 +38,27 @@ namespace _30LIDTS_RicardoCruz_05
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+        private void InsertarRegistro(string nombre, string apellidos, int edad, decimal estatura, string telefono, string genero)
+        {
+            using (MySqlConnection conection = new MySqlConnection(conexionSQL))
+            {
+                conection.Open();
+
+                string insertQuery = "INSERT INTO registros (Nombre, Apellidos, Edad, Estatura, Telefono, Genero)" + "VALUES (@Nombre, @Apellidos, @Edad, @Estatura, @Telefono, @Genero)";
+
+                using (MySqlCommand command = new MySqlCommand(insertQuery, conection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Apellidos", apellidos);
+                    command.Parameters.AddWithValue("@Edad", edad);
+                    command.Parameters.AddWithValue("@Estatura", estatura);
+                    command.Parameters.AddWithValue("@Telefono", telefono);
+                    command.Parameters.AddWithValue("@Genero", genero);
+                }
+
+                conection.Close();
+            }
         }
         private void btn_Guardar_Click(object sender, EventArgs e)
         {
@@ -78,8 +101,16 @@ namespace _30LIDTS_RicardoCruz_05
                          {
                             //Si el archivo existe, añadir un separador antes del nuevo registro
                             write.WriteLine();
+                            InsertarRegistro(nombres, apellidos, int.Parse(edad), decimal.Parse(estatura), telefono, genero);
+                            MessageBox.Show("Datos registrados correctamente");
                          }
-                          write.WriteLine(datos);
+                         else
+                         {
+                                write.WriteLine(datos);
+                                InsertarRegistro(nombres, apellidos, int.Parse(edad), decimal.Parse(estatura), telefono, genero);
+                                MessageBox.Show("Datos registrados correctamente");
+                         }
+                            
                        }
 
                     }
@@ -110,17 +141,32 @@ namespace _30LIDTS_RicardoCruz_05
             decimal resultado;
             return decimal.TryParse(valor, out resultado);
         }
-        private bool EsEnteroValidoDe10Digitos(string valor)
+        private bool EsEnteroValidoDe10Digitos(string input)
         {
-            long resultado;
-            return long.TryParse(valor, out resultado) && valor.Length == 10;
+            if(input.Length!=10)
+            {
+                return false;
+            }
+            if (!input.All(char.IsDigit))
+            {
+                return false;
+            }
+            return false;
+
         }
         private bool EsTextoValido(string valor)
         {
             return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$");                                                                                                                                                                             
         }
-
-
+        public void ValidarEdad(object sender, EventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+            if (!EsEnteroValido(textbox.Text))
+            {
+                MessageBox.Show("Por favor, ingrese una edad válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textbox.Clear();
+            }
+        }
 
         public void ValidarEstatura(object sender, EventArgs e)
         {
@@ -137,19 +183,14 @@ namespace _30LIDTS_RicardoCruz_05
         {
             TextBox textbox = (TextBox)sender;
             string input = textbox.Text;
-            if (input.Length > 10)
+            if (input.Length < 10) 
             {
-                if (!EsEnteroValidoDe10Digitos(input))
-                {
-                    MessageBox.Show("Por favor, ingrese una numero de telefono válido de 10 dígitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    textbox.Clear();
-
-                }
+                return;
             }
             else if (!EsEnteroValidoDe10Digitos(input))
             {
                 MessageBox.Show("Por favor, ingrese una numero de telefono válido de 10 dígitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                
+                textbox.Clear();
 
             }
         }
